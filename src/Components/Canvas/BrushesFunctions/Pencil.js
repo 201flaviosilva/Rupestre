657ddb87b7eace2738a8ae85d1474a1901e94205
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2020-12-18 19:29:57 +0000

    Fix Pencil draw

--- src/Components/Canvas/BrushesFunctions/Pencil.js
+++ src/Components/Canvas/BrushesFunctions/Pencil.js
@@ -2,12 +2,26 @@ export default function Pencil(ctx, position, bushOptions) {
 	const { last, actual } = position;
 	const { color, size, format } = bushOptions;
 	ctx.fillStyle = color;
-	ctx.beginPath(ctx);
 
-	if (!last.x && !last.y) {
-		if (format) ctx.rect(actual.x, actual.y, size, size);
-		else ctx.arc(actual.x, actual.y, size / 2, 0, 2 * Math.PI, false);
-		ctx.fill();
+	normalDraw({ ctx, actual, size, format });
+	// lineDraw({ ctx, last, actual, color, size, format });
+};
+
+function normalDraw({ ctx, actual, size, color, format }) {
+	ctx.beginPath(ctx);
+	if (format === "Square") {
+		ctx.rect(actual.x, actual.y, size, size);
+	} else {
+		const sizeCalc = size / 2;
+		ctx.arc(actual.x + sizeCalc, actual.y + sizeCalc, sizeCalc, 0, 2 * Math.PI, false);
+	}
+	ctx.fill();
+}
+
+function lineDraw({ ctx, last, actual, color, size, format }) {
+	ctx.beginPath(ctx);
+	if (!last.x || !last.y) {
+		normalDraw({ ctx, actual, size, color, format });
 	} else {
 		ctx.strokeStyle = color;
 		ctx.lineWidth = size;
@@ -17,6 +31,7 @@ export default function Pencil(ctx, position, bushOptions) {
 
 		ctx.moveTo(last.x, last.y);
 		ctx.lineTo(actual.x, actual.y);
-		ctx.stroke();
 	}
-};
+	ctx.stroke();
+	ctx.fill();
+}
